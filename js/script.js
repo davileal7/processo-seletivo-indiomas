5bb174f89a5e5cf62ba83420926c92bec9e7ed30
Davi Leal <davi.m.leal@gmail.com>
2023-01-03 23:35:36 -0300

    add class and indiomas

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ $(function() {
         $('.language-selected').addClass('change-BR')
         //secão
         $('#title').text('Esta página é uma Prova para trabalhar na LigaMagic')
-        $('#parágrafo').text('Desde já agradeço a oportunidade de participar deste processo seletivo e espero entrar na equipe para me desenvolver e aprender cada vez mais. Obrigado!')
-        
+        $('.p0').text('Selecione ao lado o Idioma que deseja utilizar do sistema.')
+        $('.p1').text('Indiomas')
+        $('.p2').text('Desde já agradeço a oportunidade de participar deste processo seletivo e espero entrar na equipe para me desenvolver e aprender cada vez mais. Obrigado!')
 
     });
     
@@ -17,15 +18,15 @@ $(function() {
 //Inglês
 $(function() {
     $('.en').click(function() {
-        
         $('.language-selected').text('en-US');
         $('.language-selected').removeClass('change-BR')
         $('.language-selected').removeClass('change-ES')
         $('.language-selected').addClass('change-EN')
         //secão
         $('#title').text('This page is a Proof to work in LigaMagic')
-        $('#parágrafo').text('Thank you in advance for the opportunity to participate in this selection process and I hope to join the team to develop and learn more and more. Thanks!')
-        
+        $('.p0').text('Next, select the language you want to use in the system.')
+        $('.p1').text('Languages')
+        $('.p2').text('Thank you in advance for the opportunity to participate in this selection process and I hope to join the team to develop and learn more and more. Thanks!')
         
     });
     
@@ -38,9 +39,10 @@ $(function() {
         $('.language-selected').addClass('change-ES')
         //secão
         $('#title').text('Esta página es una prueba para trabajar en LigaMagic')
-        $('#parágrafo').text('Gracias de antemano por la oportunidad de participar en este proceso de selección y espero unirme al equipo para desarrollarme y aprender más y más. Gracias!')
-    
-        
+        $('.p0').text('A continuación, seleccione el idioma que desea utilizar en el    sistema.')
+        $('.p1').text('Idiomas')
+        $('.p2').text('Gracias de antemano por la oportunidad de participar en este proceso de selección y espero unirme al equipo para desarrollarme y aprender más y más. Gracias!')
+
     });
     
 });
