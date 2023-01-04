9e3e71b4303757c45e22bbd52a440c8423a2126a
Davi Leal <davi.m.leal@gmail.com>
2023-01-04 12:40:56 -0300

    Finalizado

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ $(function() {
         $('.language-selected').removeClass('change-ES')
         $('.language-selected').addClass('change-EN')
         //secão
-        $('#title').text('This page is a Proof to work in LigaMagic')
+        $('#title').text('This page is a Proof to work in LigaMagic.')
         $('.p0').text('Next, select the language you want to use in the system.')
         $('.p1').text('Languages')
         $('.p2').text('Thank you in advance for the opportunity to participate in this selection process and I hope to join the team to develop and learn more and more. Thanks!')
@@ -40,7 +40,7 @@ $(function() {
         $('.language-selected').text('es-ES');
         $('.language-selected').addClass('change-ES')
         //secão
-        $('#title').text('Esta página es una prueba para trabajar en LigaMagic')
+        $('#title').text('Esta página es una prueba para trabajar en LigaMagic.')
         $('.p0').text('A continuación, seleccione el idioma que desea utilizar en el    sistema.')
         $('.p1').text('Idiomas')
         $('.p2').text('Gracias de antemano por la oportunidad de participar en este proceso de selección y espero unirme al equipo para desarrollarme y aprender más y más. Gracias!')
