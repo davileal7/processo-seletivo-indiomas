eba2e2f2a81145639d92ce72c042f10cb23c6667
Davi Leal <davi.m.leal@gmail.com>
2023-01-04 12:19:42 -0300

    border radius, footer

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ $(function() {
         $('.p0').text('Selecione ao lado o Idioma que deseja utilizar do sistema.')
         $('.p1').text('Indiomas')
         $('.p2').text('Desde já agradeço a oportunidade de participar deste processo seletivo e espero entrar na equipe para me desenvolver e aprender cada vez mais. Obrigado!')
+        $('footer').text('Davi Moreira Leal - candidato à vaga de estagiário em PHP.')
 
     });
     
@@ -27,6 +28,7 @@ $(function() {
         $('.p0').text('Next, select the language you want to use in the system.')
         $('.p1').text('Languages')
         $('.p2').text('Thank you in advance for the opportunity to participate in this selection process and I hope to join the team to develop and learn more and more. Thanks!')
+        $('footer').text('Davi Moreira Leal - candidate for the position of intern in PHP.')
         
     });
     
@@ -42,6 +44,7 @@ $(function() {
         $('.p0').text('A continuación, seleccione el idioma que desea utilizar en el    sistema.')
         $('.p1').text('Idiomas')
         $('.p2').text('Gracias de antemano por la oportunidad de participar en este proceso de selección y espero unirme al equipo para desarrollarme y aprender más y más. Gracias!')
+        $('footer').text('Davi Moreira Leal - candidato a la posición de pasante en PHP.')
 
     });
     
